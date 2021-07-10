e7bc6128665eddb5f502f22d1b71db2a473be502
mepau <mepau>
2021-07-09 22:15:36 -0500

    Implemented custom genre_picker.dart instead of multi_select

--- lib/screens/query_screen.dart
+++ lib/screens/query_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import "../widgets/genres_picker.dart";
 import "./results_screen.dart";
+import "../models/genre.dart";
 
-class QueryScreen extends StatelessWidget {
+class QueryScreen extends StatefulWidget {
   const QueryScreen({Key? key}) : super(key: key);
 
   @override
+  _QueryScreenState createState() => _QueryScreenState();
+}
+
+class _QueryScreenState extends State<QueryScreen> {
+  List<Genre> _selectedGenres = [];
+
+  set list(List<Genre> value) => setState(() => _selectedGenres = value);
+
+  @override
   Widget build(BuildContext context) {
-    print(key);
+    print(_selectedGenres);
     return Scaffold(
       appBar: AppBar(
         title: Text("Hello"),
@@ -16,7 +26,8 @@ class QueryScreen extends StatelessWidget {
         child: Container(
           child: Column(
             children: [
-              GenresPicker(),
+              GenresPicker(
+                  callback: (val) => setState(() => _selectedGenres = val)),
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
@@ -25,7 +36,10 @@ class QueryScreen extends StatelessWidget {
                   );
                 },
                 child: Text("Submit"),
-              )
+              ),
+              _selectedGenres.length > 0
+                  ? Text(_selectedGenres[0].name)
+                  : Text("hello"),
             ],
           ),
         ),
@@ -33,3 +47,35 @@ class QueryScreen extends StatelessWidget {
     );
   }
 }
+
+//class QueryScreen extends StatelessWidget {
+//  const QueryScreen({Key? key}) : super(key: key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    print(key);
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text("Hello"),
+//      ),
+//      body: SingleChildScrollView(
+//        child: Container(
+//          child: Column(
+//            children: [
+//              GenresPicker(),
+//              ElevatedButton(
+//                onPressed: () {
+//                  Navigator.push(
+//                    context,
+//                    MaterialPageRoute(builder: (context) => ResultsScreen()),
+//                  );
+//                },
+//                child: Text("Submit"),
+//              )
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
--- lib/widgets/genres_picker.dart
+++ lib/widgets/genres_picker.dart
@@ -3,20 +3,36 @@ import 'package:multi_select_flutter/multi_select_flutter.dart';
 import "../models/genre.dart";
 import "../providers/requests/rapid_api_req.dart";
 
+typedef void ListCallback(List<Genre> val);
+
 class GenresPicker extends StatefulWidget {
+  final ListCallback callback;
+  GenresPicker({required this.callback});
+
   @override
-  _GenresPickerState createState() => _GenresPickerState();
+  _GenresPickerState createState() => _GenresPickerState(callback: callback);
 }
 
 class _GenresPickerState extends State<GenresPicker> {
+  final ListCallback callback;
+  _GenresPickerState({required this.callback});
   var _items;
 
   List<Genre> _selectedGenres = [];
+  List<Genre> _filteredGenres = [];
 
   @override
   void initState() {
     super.initState();
   }
+
+  addGenre(genre) {
+    setState(() {
+      _selectedGenres.add(genre);
+    });
+  }
+
+  static String _displayStringForOption(Genre option) => option.name;
 
   @override
   Widget build(BuildContext context) {
@@ -27,57 +43,160 @@ class _GenresPickerState extends State<GenresPicker> {
         future: fetchGenres(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            _items = snapshot.data!
-                .map((genre) => MultiSelectItem<Genre>(genre, genre.name))
-                .toList();
-            return Column(
-              children: <Widget>[
-                //################################################################################################
-                // This MultiSelectBottomSheetField has no decoration, but is instead wrapped in a Container that has
-                // decoration applied. This allows the ChipDisplay to render inside the same Container.
-                //################################################################################################
-                Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor.withOpacity(.1),
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
+            return Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "MODAL BOTTOM SHEET EXAMPLE",
+                    style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        letterSpacing: 0.4,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return StatefulBuilder(
+                              builder: (context, setNewState) {
+                            return Column(
+                              children: <Widget>[
+                                Autocomplete<Genre>(
+                                  displayStringForOption:
+                                      _displayStringForOption,
+                                  optionsBuilder:
+                                      (TextEditingValue textEditingValue) {
+                                    if (textEditingValue.text == "") {
+                                      return const Iterable<Genre>.empty();
+                                    }
+                                    setNewState(() {
+                                      _filteredGenres =
+                                          snapshot.data!.where((Genre option) {
+                                        return option.name
+                                                .toLowerCase()
+                                                .contains(textEditingValue.text
+                                                    .toLowerCase()) ||
+                                            option.name
+                                                .toLowerCase()
+                                                .startsWith(
+                                                    textEditingValue.text);
+                                      }).toList();
+                                    });
+                                    return snapshot.data!.where(
+                                      (Genre option) {
+                                        return option.name
+                                                .toLowerCase()
+                                                .contains(textEditingValue.text
+                                                    .toLowerCase()) ||
+                                            option.name
+                                                .toLowerCase()
+                                                .startsWith(
+                                                    textEditingValue.text);
+                                      },
+                                    );
+                                  },
+                                  onSelected: (Genre selection) {
+                                    setNewState(() {
+                                      _selectedGenres.add(selection);
+                                    });
+                                  },
+                                ),
+                                Text("Selected Genres:"),
+                                SizedBox(
+                                  height: 60,
+                                  child: GridView.builder(
+                                    gridDelegate:
+                                        SliverGridDelegateWithMaxCrossAxisExtent(
+                                            maxCrossAxisExtent: 100,
+                                            childAspectRatio: 4 / 2,
+                                            crossAxisSpacing: 10,
+                                            mainAxisSpacing: 10),
+                                    itemCount: _selectedGenres.length,
+                                    itemBuilder: (BuildContext ctx, index) {
+                                      return Container(
+                                        alignment: Alignment.center,
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            setNewState(() {
+                                              _selectedGenres.remove(
+                                                  _selectedGenres[index]);
+                                            });
+                                          },
+                                          child:
+                                              Text(_selectedGenres[index].name),
+                                        ),
+                                        decoration: BoxDecoration(
+                                            color: Colors.blue[200],
+                                            borderRadius:
+                                                BorderRadius.circular(15)),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                Text("Genres:"),
+                                Expanded(
+                                  child: GridView.builder(
+                                    gridDelegate:
+                                        SliverGridDelegateWithMaxCrossAxisExtent(
+                                            maxCrossAxisExtent: 100,
+                                            childAspectRatio: 4 / 2,
+                                            crossAxisSpacing: 10,
+                                            mainAxisSpacing: 10),
+                                    itemCount: snapshot.data!.length,
+                                    itemBuilder: (BuildContext ctx, index) {
+                                      return Container(
+                                        alignment: Alignment.center,
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            if (!_selectedGenres.contains(
+                                                snapshot.data![index])) {
+                                              setNewState(() {
+                                                _selectedGenres
+                                                    .add(snapshot.data![index]);
+                                              });
+                                            } else {
+                                              setNewState(() {
+                                                _selectedGenres.remove(
+                                                    snapshot.data![index]);
+                                              });
+                                            }
+                                          },
+                                          child:
+                                              Text(snapshot.data![index].name),
+                                        ),
+                                        decoration: BoxDecoration(
+                                            color: _selectedGenres.contains(
+                                                    snapshot.data![index])
+                                                ? Colors.blue[200]
+                                                : Colors.blue[50],
+                                            borderRadius:
+                                                BorderRadius.circular(15)),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
+                            );
+                          });
+                        },
+                      );
+                    },
+                    child: Text(
+                      'Click Me',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.6),
                     ),
                   ),
-                  child: Column(
-                    children: <Widget>[
-                      MultiSelectBottomSheetField(
-                        initialChildSize: 0.4,
-                        listType: MultiSelectListType.CHIP,
-                        searchable: true,
-                        buttonText: Text("Favorite Genres"),
-                        title: Text("Genres"),
-                        items: _items,
-                        onConfirm: (values) {
-                          _selectedGenres = values.cast();
-                        },
-                        chipDisplay: MultiSelectChipDisplay(
-                          onTap: (value) {
-                            setState(() {
-                              _selectedGenres.remove(value);
-                            });
-                          },
-                        ),
-                      ),
-                      _selectedGenres.length == 1
-                          ? Container(
-                              padding: EdgeInsets.all(10),
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                "None selected",
-                                style: TextStyle(color: Colors.black54),
-                              ),
-                            )
-                          : Container(),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             );
           } else if (snapshot.hasError) {
             return Text("${snapshot.error}");
